cf4b7192a79c4eebe2dd55c8a0b691f433494d0d
Asim <asimpsy11@gmail.com>
2022-11-27 09:57:50 +0100

    MainActivity class cleaned and improved

--- app/src/main/java/com/app/stationsapp/MainActivity.kt
+++ app/src/main/java/com/app/stationsapp/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -23,6 +22,7 @@ import com.app.stationsapp.model.StationData
 import com.app.stationsapp.ui.theme.StationsAppTheme
 import com.app.stationsapp.viewmodel.StationsViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
@@ -38,101 +38,72 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = hiltViewModel<StationsViewModel>()
-
-            val multiplePermissionState = rememberMultiplePermissionsState(
-                permissions = listOf(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            )
-
-            val cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(LatLng(52.087966, 5.113372), 12f)
-            }
-
-            val refreshMarkers = {
-                viewModel.stationsWithin(cameraPositionState.projection?.visibleRegion?.latLngBounds)
-            }
-
-            if (!cameraPositionState.isMoving) {
-                refreshMarkers()
-            }
-
-            LaunchedEffect(Unit) {
-                multiplePermissionState.launchMultiplePermissionRequest()
-            }
-
             StationsAppTheme {
-
-                val sheetState = rememberBottomSheetState(
-                    initialValue = BottomSheetValue.Collapsed
-                )
-                val scaffoldState = rememberBottomSheetScaffoldState(
-                    bottomSheetState = sheetState
-                )
-
-                BottomSheetScaffold(
-                    scaffoldState = scaffoldState,
-                    sheetContent = {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(300.dp),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            val listState = rememberLazyListState()
-                            LazyColumn(
-                                modifier = Modifier.fillMaxSize(),
-                                state = listState,
-                                verticalArrangement = Arrangement.spacedBy(5.dp),
-                                contentPadding = PaddingValues(10.dp)
-                            ) {
-                                items(viewModel.allStationsList) { item ->
-                                    ListItem(stationData = item)
-                                }
-                            }
-                        }
-                    },
-                    sheetPeekHeight = 0.dp
-                ) {
-                    if (multiplePermissionState.allPermissionsGranted) {
-                        GoogleMap(
-                            cameraPositionState = cameraPositionState,
-                            properties = MapProperties(isMyLocationEnabled = true),
-                            uiSettings = MapUiSettings(compassEnabled = true),
-                            onMapLoaded = refreshMarkers
-                        ) {
-                            viewModel.stationsList.forEach { station ->
-                                Marker(
-                                    state = rememberMarkerState(position = LatLng(station.lat, station.lng)),
-                                    title = station.namen.kort,
-                                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-                                )
-                            }
-                        }
-                    }
-                }
-
-                val scope = rememberCoroutineScope()
-
-                FloatingActionButton(
-                    backgroundColor = Color.Red,
-                    modifier = Modifier.padding(12.dp),
-                    onClick = {
-                        scope.launch {
-                            if (sheetState.isCollapsed) {
-                                sheetState.expand()
-                            } else {
-                                sheetState.collapse()
-                            }
-                        }
-                    }
-                ) {
-                    Text(text = "Stations", modifier = Modifier.padding(5.dp), color = Color.White)
-                }
+                MainApp(viewModel = hiltViewModel())
             }
         }
+    }
+}
+
+@ExperimentalMaterialApi
+@ExperimentalPermissionsApi
+@Composable
+fun MainApp(viewModel: StationsViewModel) {
+
+    val multiplePermissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    )
+
+    val cameraPositionState = rememberCameraPositionState { position = CameraPosition.fromLatLngZoom(LatLng(52.087966, 5.113372), 12f) }
+    if (!cameraPositionState.isMoving) viewModel.stationsWithin(cameraPositionState.projection?.visibleRegion?.latLngBounds)
+
+    LaunchedEffect(Unit) { multiplePermissionState.launchMultiplePermissionRequest() }
+
+    val sheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = sheetState)
+
+    StationsBottomSheet(
+        scaffoldState = scaffoldState,
+        viewModel = viewModel,
+        multiplePermissionState = multiplePermissionState,
+        cameraPositionState = cameraPositionState
+    )
+
+    AllStationsButton(sheetState = sheetState)
+}
+
+@ExperimentalPermissionsApi
+@ExperimentalMaterialApi
+@Composable
+fun StationsBottomSheet(scaffoldState: BottomSheetScaffoldState, viewModel: StationsViewModel, multiplePermissionState: MultiplePermissionsState, cameraPositionState: CameraPositionState) {
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                val listState = rememberLazyListState()
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    state = listState,
+                    verticalArrangement = Arrangement.spacedBy(5.dp),
+                    contentPadding = PaddingValues(10.dp)
+                ) {
+                    items(viewModel.allStationsList) { item ->
+                        ListItem(stationData = item)
+                    }
+                }
+            }
+        },
+        sheetPeekHeight = 0.dp
+    ) {
+        StationsMap(multiplePermissionState = multiplePermissionState, viewModel = viewModel, cameraPositionState = cameraPositionState)
     }
 }
 
@@ -149,19 +120,47 @@ fun ListItem(stationData: StationData) {
     }
 }
 
+@ExperimentalMaterialApi
 @Composable
-fun GoogleMarkers() {
-    Marker(
-        state = rememberMarkerState(position = LatLng(52.087966, 5.113372)),
-        title = "Marker1",
-        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
-    )
+fun AllStationsButton(sheetState: BottomSheetState) {
+    val scope = rememberCoroutineScope()
+
+    FloatingActionButton(
+        backgroundColor = Color.Red,
+        modifier = Modifier.padding(12.dp),
+        onClick = {
+            scope.launch {
+                if (sheetState.isCollapsed) {
+                    sheetState.expand()
+                } else {
+                    sheetState.collapse()
+                }
+            }
+        }
+    ) {
+        Text(text = "Stations", modifier = Modifier.padding(5.dp), color = Color.White)
+    }
 }
 
-@Preview(showBackground = true)
+@ExperimentalPermissionsApi
 @Composable
-fun DefaultPreview() {
-    StationsAppTheme {
-        //Greeting("Android")
+fun StationsMap(multiplePermissionState: MultiplePermissionsState, viewModel: StationsViewModel, cameraPositionState: CameraPositionState) {
+    if (multiplePermissionState.allPermissionsGranted) {
+        GoogleMap(
+            cameraPositionState = cameraPositionState,
+            properties = MapProperties(isMyLocationEnabled = true),
+            uiSettings = MapUiSettings(compassEnabled = true),
+            onMapLoaded = {
+                viewModel.stationsWithin(cameraPositionState.projection?.visibleRegion?.latLngBounds)
+            }
+        ) {
+            viewModel.stationsList.forEach { station ->
+                Marker(
+                    state = rememberMarkerState(position = LatLng(station.lat, station.lng)),
+                    title = station.namen.kort,
+                    icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
+                )
+            }
+        }
     }
 }
